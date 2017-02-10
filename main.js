be438dcc7737246790730e5273bf5c4368a7c993
Matkins15 <matthew1atkins@gmail.com>
2017-02-10 16:54:47 -0500

    updated js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
+
 var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
 
-if (cardThree === cardFour) {
-	alert("You found a match!")
-}	else (cardThree!== cardFour) {
-	alert("Sorry try again")
+if(cardThree === cardFour) {
+	alert('You found a match!');
+}	else if(cardOne!== cardThree) {
+	alert('Sorry try again');
 }
